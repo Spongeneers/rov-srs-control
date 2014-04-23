4a3d56589cbf4e94618795d3f1bc09fa0f59e5ca
ROV SRS BeagleBone <2014.45x.spongeneering@gmail.com>
2014-04-23 21:12:49 +0000

    Add "ROV_SRS_Library.py" file containing functions for main script.

---
+++ ROV_SRS_Library.py
@@ -0,0 +1,29 @@
+# ROV_SRS_Library
+#
+#
+# Overview:	A collection of helper functions used by the BeagleBone
+#		to control the ROV SRS Actuators.
+#
+# Authors:	Jonathan Lee (2015)
+#
+
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.PWM as PWM
+
+def calc_pulse_width(pin_name):
+	"""Calculates the pulse width of a PWM signal input.
+
+	Stores the time of day on a Rising Edge and subsequent Falling Edge
+	event, then returns the difference in milliseconds.
+
+	Args:
+		pin_name: A String containing the pin name on which the PWM
+			signal is expected. The pin name should be in the
+			format defined by the Adafruit_BBIO library.
+	
+	Returns:
+		A float containing the calculated pulse width, in
+			milliseconds.
+	"""
+
+	
