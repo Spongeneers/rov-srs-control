abac71386b95b7b9dca4d2e45461bd34b8f5a076
vjon <vjon207@gmail.com>
2015-03-26 10:42:34 -0700

    Add "ROV_SRS_Main.py"
    Added file for main runtime script.

--- ROV_SRS_Main.py
+++ ROV_SRS_Main.py
@@ -12,6 +12,26 @@
 #
 
 import ROV_SRS_Library as ROV
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.PWM as PWM
+
+# Define Module-level Constants.
+#
+# Refer to the "Adafruit_BBIO" documentation regarding acceptable naming
+# formats for Pins.
+
+# General Program Constants.
+PULSE_NUM_AVG = 5			# Number of Pulses to average before state transitions.
+PULSE_WID_HI = 0.002		# Input Pulse Width (70 Hz, 14% Duty Cycle) [msec].
+PULSE_WID_LO = 0.001		# Input Pulse Width (70 Hz, 7% Duty Cycle) [msec].
+PULSE_WID_TOL = 0.00025		# Tolerance on Pulse Width Deviations [msec].
+PULSE_WID_LIM = 0.014		# Upper Limit on Pulse Widths (70 Hz, 1 Period) [msec].
+
+# Linear Actuator related Constants.
+LIN_IN1 = "P8_12"		# Pin for Input PWM from RC Controller.
+LIN_ENA = "P9_16"		# Pin for Output High Enable Signal.
+LIN_OUT1 = "P9_14"		# Pin for Output High/Low to Driver CH1.
+LIN_OUT2 = "P9_12"		# Pin for Output High/Low to Driver CH2.
 
 def main():
 	"""Translates RC Controller Input to appropriate actuator signals.
@@ -29,4 +49,5 @@ def main():
 	Returns:
 		N/A
 	"""
-
+	
+	# Define Constant parameters.
