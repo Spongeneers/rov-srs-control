97a1e71c1c3961725c9153091d6b0299c2fd3bb7
vjon <vjon207@gmail.com>
2015-04-19 00:22:22 -0700

    Refactor Library functions
    Extract position command trend check into separate function, setup
    function headers for Carousel and Shoulder stepper motors.

--- ROV_SRS_Library.py
+++ ROV_SRS_Library.py
@@ -15,8 +15,17 @@ import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 import time
 
-def calc_pulse_width(pin_name, avg_num, sig_freq):
-	"""Calculate the pulse width of a PWM signal input.
+#
+# Constant Definitions.
+#
+
+# Hardware-related Constants.
+LA_STROKE_TIME = 2		# Firgelli L16-P: Full stroke time [sec].
+CS_STEP_ANGLE = 1.8		# Soyo Unipolar: Step Angle [degrees].
+SS_STEP_ANGLE = 1.8		# Soyo Unipolar: Step Angle [degrees].
+
+def calc_width(pin, size, freq):
+	"""Calculate the Pulse Width of a PWM input signal.
 
 	This function stores the time of day on a Rising Edge and subsequent 
 	Falling Edge event, calculates the average of multiple such events, 
@@ -24,155 +33,224 @@ def calc_pulse_width(pin_name, avg_num, sig_freq):
 	on 0 or 100% Duty Cycle signals.
 
 	Args:
-		pin_name:		A String specifying the pin name on which the PWM 
-						signal is expected. The pin name should be in the 
-						format defined by the Adafruit_BBIO library.
-		avg_num:		An Integer specifying the sample size.
-		sig_freq:		An Integer specifying the expected frequency of 
-						the PWM signal [Hz].
+		pin:		A String specifying the pin name on which the PWM 
+					signal is expected. The pin name should be in the 
+					format defined by the Adafruit_BBIO library.
+		size:		An Integer specifying the sample size.
+		freq:		An Integer specifying the expected frequency of 
+					the PWM signal [Hz].
 	
 	Returns:
-		pulse_avg:		The calculated pulse width [milliseconds].
+		avg:		The calculated Pulse Width [milliseconds].
 	"""
-	pulse_avg = 0.0
+	avg = 0.0
 	__rise_flag = 0.0
 	__fall_flag = 0.0
 	__pulse_width = 0.0
 	
-	for i in range(avg_num)
+	for i in range(size)
 		# Determine incoming signal Pulse Width.
-		GPIO.wait_for_edge(pin_name, GPIO.RISING)
+		GPIO.wait_for_edge(pin, GPIO.RISING)
 		__rise_flag = time.time()
-		GPIO.wait_for_edge(pin_name, GPIO.FALLING)
+		GPIO.wait_for_edge(pin, GPIO.FALLING)
 		__fall_flag = time.time()
 		__pulse_width = __fall_flag - __rise_flag
 		
 		# Adjust for missed Pulses (CPU busy during Edge event).
 		# This is done by subtracting the PWM period value if the
 		# measured pulse width is found to be greater than the period.
-		while __pulse_width > (1/signal_freq)
-			__pulse_width -= (1/signal_freq)
+		while __pulse_width > (1/freq)
+			__pulse_width -= (1/freq)
 		
-		pulse_avg += __pulse_width
+		avg += __pulse_width
 	
-	return (pulse_avg /= avg_num)
+	return (avg /= size)
 
-def set_position(pwm_width, sig_freq, sig_max, sig_min, sig_tol):
-	"""Set the position command corresponding to a PWM pulse width.
+def set_position(width, freq, max, min, tol):
+	"""Set the Position Command corresponding to a PWM Pulse Width.
 	
-	This function checks if the pulse width value provided falls within
-	the tolerance range of the expected maximum or minimum pulse width
-	values and returns the corresponding position command.
+	This function checks if the Pulse Width value provided falls within
+	the tolerance range of the expected maximum or minimum Pulse Width
+	values and returns the corresponding Position Command.
 	
 	Args:
-		pwm_width:		A float specifying the measured pulse width
-						[milliseconds].
-		sig_freq:		An integer specifying the expected frequency
-						of the PWM signal [Hz].
-		sig_max:		An integer specifying the maximum expected
-						duty cycle of the PWM signal [%].
-		sig_min:		An integer specifying the minimum expected
-						duty cycle of the PWM signal [%].
-		sig_tol:		An integer specifying the acceptable tolerance
-						on deviations in the measured pulse width from
-						the expected values. The excepted values
-						correspond to the pulse widths of the maximum
-						and minimum expected duty cycle values specified
-						above. The tolerance is expressed relative to
-						the millisecond difference between the maximum 
-						and minimum possible pulse widths [%].
+		width:		A float specifying the measured Pulse Width
+					[milliseconds].
+		freq:		An integer specifying the expected frequency
+					of the PWM signal [Hz].
+		max:		An integer specifying the maximum expected
+					duty cycle of the PWM signal [%].
+		min:		An integer specifying the minimum expected
+					duty cycle of the PWM signal [%].
+		tol:		An integer specifying the acceptable tolerance
+					on deviations in the measured Pulse Width from
+					the expected values. The excepted values
+					correspond to the Pulse Widths of the maximum
+					and minimum expected duty cycle values specified
+					above. The tolerance is expressed relative to
+					the millisecond difference between the maximum 
+					and minimum possible Pulse Widths [%].
 	
 	Returns:
-		pos_cmd:		The position command corresponding to the
-						measured pulse width.
-						2  == maximum pulse width.
-						1  == intermediate pulse width.
-						0  == minimum pulse width.
-						-1 == error value
+		cmd:		The Position Command corresponding to the
+					measured Pulse Width.
+					2  == Maximum Pulse Width.
+					1  == Intermediate Pulse Width .
+					0  == Minimum Pulse Width.
+					-1 == Error.
 	"""
-	pos_cmd = 0
-	__pwm_width_max = ((1/signal_freq)*(sig_max/100))
-	__pwm_width_min = ((1/signal_freq)*(sig_min/100))
-	__pwm_width_tol = (__pwm_width_max - __pwm_width_min)*(sig_tol/100)
+	cmd = -1
+	__pwm_width_max = ((1/freq)*(max/100))
+	__pwm_width_min = ((1/freq)*(min/100))
+	__pwm_width_tol = (__pwm_width_max - __pwm_width_min)*(tol/100)
 	
 	# Check absolute maximum allowable value.
-	if pwm_width < (__pwm_width_max + __pwm_width_tol)
+	if width < (__pwm_width_max + __pwm_width_tol)
 		# Check against multiple minimum values.
-		if pwm_width >= (__pwm_width_max - __pwm_width_tol)
-			pos_cmd = 2
-		elif pwm_width >= (__pwm_width_min + __pwm_width_tol)
-			pos_cmd = 1
-		elif pwm_width >= (__pwm_width_min - __pwm_width_tol)
-			pos_cmd = 0
-		else
-			pos_cmd = -1
-	else
-		pos_cmd = -1
+		if width >= (__pwm_width_max - __pwm_width_tol)
+			cmd = 2
+		elif width >= (__pwm_width_min + __pwm_width_tol)
+			cmd = 1
+		elif width >= (__pwm_width_min - __pwm_width_tol)
+			cmd = 0
 	
-	return pos_cmd
+	return cmd
 
-def move_linear(la_hist, hist_len, la_out1, la_out2, stroke):
-	"""Move the linear actuator to match the desired position command.
+def check_trend(hist, length):
+	"""Check for Position Command persistance.
 	
-	This function checks for a transition within a series of position
-	commands and changes the position of the linear actuator when the
-	most recent command has persisted for half the length of the position 
-	command history series.
+	This function returns the most recent in a series of Position
+	Commands if the series exhibits a 50/50 split between two different
+	types of Position Commands.
 	
 	Args:
-		la_hist:		A deque containing the history of position
-						commands, which are themselves integers with
-						values between -1 and 2. Refer to set_position()
-						Returns: "pos_cmd" for further details.
-		hist_len:		An integer specifying the length of the "la_hist"
-						deque.
-		la_out1:		A string specifying the pin name on which the first
-						output signal should be sent.
-		la_out2:		A string specifying	the pin name on which the second
-						output signal should be sent.
-		stroke:			A float specifying the stroke fraction desired for
-						the shaft movement.
+		hist:		A deque containing the history of Position
+					Commands, which are themselves integers with
+					values between -1 and 2. Refer to function Returns
+					for further details.
+		length:		An integer specifying the length of the "hist" deque.
+	
+	Returns:
+		trend:		The persistant value of the Command series 2nd half.
+					2  == Maximum Pulse Width.
+					1  == Intermediate Pulse Width.
+					0  == Minimum Pulse Width.
+					-1 == Error.
+	"""
+	trend = -1
+	__start_flag = True
+	__end_flag = True
+	__start_index = 0
+	__end_index = length
+	
+	# Check for duration of oldest Command.
+	while __start_flag is True and __start_index < length
+		if hist[__start_index] is hist[0]
+			__start_index += 1
+		else
+			__start_flag = False
+	
+	# Check for duration of newest Command.
+	while __end_flag is True and __end_index >= 0
+		if hist[__end_index] is hist[length]
+			__end_index -= 1
+		else
+			__end_flag = False
+			
+	# Check for 50/50 command split.
+	if start_flag is True and end_flag is True
+		trend = hist[length]
+	
+	return trend
+	
+def move_linear(cmd, out1, out2, stroke):
+	"""Move the Linear Actuator to match the desired Position Command.
+	
+	This function changes the Position of the Linear Actuator in
+	accordance with the input Position Command.
+	
+	Args:
+		cmd:		The Position Command to enact.
+					2  == Retract Linear Actuator (Open Gripper).
+					1  == Lock current Position.
+					0  == Extend Linear Actuator (Close Gripper).
+					-1 == Error
+		out1:		A string specifying the pin name on which the Extend
+					output signal should be sent.
+		out2:		A string specifying	the pin name on which the Retract
+					output signal should be sent.
+		stroke:		A float specifying the stroke fraction desired for
+					the shaft movement.
 	
 	Returns:
 		N/A
 	"""
-	max_stroke_time = 2		# Experimentally determined for the Firgelli
-							#	L16-P Linear Actuator
-	
-	end_flag = True
-	end_index = hist_len
-	
-	# Check for duration of newest Command.
-	while end_flag is True and end_index >= 0
-		if la_hist[end_index] is la_hist[hist_len]
-			end_index -= 1
-		else
-			end_flag = False
-	
-	start_flag = True
-	start_count = 0
-	
-	# Check for duration of oldest Command.
-	while start_flag is True and start_index < hist_len
-		if la_hist[start_index] is la_hist[0]
-			start_index += 1
-		else
-			start_flag = False
-			
-	# Change Linear Actuator Position if Command History meets criteria.
-	if start_flag is True and end_flag is True
-		if la_hist[hist_len] is 2
-			# Retract Linear Actuator for specific amount of time.
-			GPIO.output(PIN_LA_OUT1, GPIO.LOW)
-			GPIO.output(PIN_LA_OUT2, GPIO.HIGH)
-			time.sleep(max_stroke_time*stroke)
-		elif la_hist[hist_len] is 0
-			# Extend Linear Actuator for full amount of time.
-			GPIO.output(PIN_LA_OUT1, GPIO.HIGH)
-			GPIO.output(PIN_LA_OUT2, GPIO.LOW)
-			time.sleep(max_stroke_time)
-	
+	if cmd is 2
+		# Retract Linear Actuator for specific amount of time.
+		GPIO.output(out1, GPIO.LOW)
+		GPIO.output(out2, GPIO.HIGH)
+		time.sleep(LA_STROKE_TIME*stroke)
+	elif cmd is 0
+		# Extend Linear Actuator for full amount of time.
+		GPIO.output(out1, GPIO.HIGH)
+		GPIO.output(out2, GPIO.LOW)
+		time.sleep(LA_STROKE_TIME)
+	# TODO: Error handling.
+
 	# Hold Linear Actuator at desired Position.
-	GPIO.output(PIN_LA_OUT1, GPIO.LOW)
-	GPIO.output(PIN_LA_OUT2, GPIO.LOW)
+	GPIO.output(out1, GPIO.LOW)
+	GPIO.output(out2, GPIO.LOW)
 	
+def move_carousel(cmd, out1, out2, out3, out4, angle):
+	"""Move the Carousel Stepper to match the desired Position Command.
+	
+	This function changes the Position of the Carousel Stepper in
+	accordance with the input Position Command.
+	
+	Args:
+		cmd:		The Position Command to enact.
+					2  == Advance Carousel Stepper (Change Gripper).
+					1  == Lock current Position.
+					0  == Lock current Position.
+					-1 == Error.
+		out1:		A string specifying the pin name on which the Phase A
+					Switch 1 signal should be sent.
+		out2:		A string specifying	the pin name on which the Phase A
+					Switch 2 signal should be sent.
+		out3:		A string specifying the pin name on which the Phase B
+					Switch 1 signal should be sent.
+		out4:		A string specifying	the pin name on which the Phase B
+					Switch 2 signal should be sent.
+		angle:		A float specifying the circle fraction desired for
+					the shaft rotation.
+	
+	Returns:
+		N/A
+	"""
+
+def move_shoulder(cmd, out1, out2, out3, out4, step):
+	"""Move the Shoulder Stepper to match the desired Position Command.
+	
+	This function changes the Position of the Shoulder Stepper in
+	accordance with the input Position Command.
+	
+	Args:
+		cmd:		The Position Command to enact.
+					2  == Step Clockwise (Tilt Down).
+					1  == Lock current Position.
+					0  == Step Counter-Clockwise (Tilt Up).
+					-1 == Error.
+		out1:		A string specifying the pin name on which the Phase A
+					Switch 1 signal should be sent.
+		out2:		A string specifying	the pin name on which the Phase A
+					Switch 2 signal should be sent.
+		out3:		A string specifying the pin name on which the Phase B
+					Switch 1 signal should be sent.
+		out4:		A string specifying	the pin name on which the Phase B
+					Switch 2 signal should be sent.
+		step:		An integer specifying the next Phase sequence for
+					continued rotation.
+	
+	Returns:
+		N/A
+	"""
